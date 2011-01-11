03466d6fa7c186eb8ca37c0fde2866c39262e98c
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2011-01-11 11:38:14 -0500

    Deleted dead push code (will reintroduce once it is being used) Added api helper functions to parse arguments Separated calendar and news modules from the web ui modules

--- future/lib/api/news.php
+++ future/lib/api/news.php
@@ -2,71 +2,73 @@
 
 /****************************************************************
  *
+ *  Copyright 2010 The President and Fellows of Harvard College
  *  Copyright 2010 Modo Labs Inc.
  *
  *****************************************************************/
 
-$maxPerPage      = $GLOBALS['siteConfig']->getVar('NEWS_MAX_RESULTS');
+require_once realpath(LIB_DIR.'/api.php');
 
-$module = Module::factory('news', '', $_GET);
+$feeds      = $GLOBALS['siteConfig']->loadFeedData('news');
+$maxPerPage = $GLOBALS['siteConfig']->getVar('NEWS_MAX_RESULTS');
+
+function getFeed($feeds, $index) {
+  if (isset($feeds[$index])) {
+    $feedData = $feeds[$index];
+    $controller = RSSDataController::factory($feedData);
+    $controller->setDebugMode($GLOBALS['siteConfig']->getVar('DATA_DEBUG'));
+    return $controller;
+  } else {
+    error_log("Error getting news feed for index $index");
+    return null;
+  }
+}
 
 $content = "";
 
-if (isset($_REQUEST['command'])) {
-
-  switch($_REQUEST['command']) {
-   case 'channels':
-     $feeds = $module->getFeeds();
-     $feed_labels = array();
-     foreach ($feeds as $feedData) {
-        $feed_labels[] = $feedData['TITLE'];
-     }
-
-     $content = json_encode($feed_labels);
-     break;
-
-   case 'search':
-     if (isset($_REQUEST['q']) && ($searchTerms = $_REQUEST['q'])) {
-        $channel = isset($_REQUEST['channel']) ? $_REQUEST['channel'] : 0;
-
-        
-        $feed = $module->getFeed($channel);
-
-       $lastStoryId = isset($_REQUEST['storyId']) ? $_REQUEST['storyId'] : NULL;
-       if ($lastStoryId) {
-           // TODO: this has not been handled yet. I need more info on when this is used
-          break;
-       }
-
-       $feed->addFilter('search', $searchTerms);
-       $content = $feed->getData(); // this returns everything.......
-
-    }       
-     break;
-
-   default:
-     break;
-  }
-
-} else {
-    $channel = isset($_REQUEST['channel']) ? $_REQUEST['channel'] : 0;
+switch(apiGetArg('command')) {
+  case 'channels':
+    $feed_labels = array();
+    foreach ($feeds as $feedData) {
+      $feed_labels[] = $feedData['TITLE'];
+    }
   
-        $feed = $module->getFeed($channel);
+    $content = json_encode($feed_labels);
+    break;
+  
+  case 'search':
+    $searchTerms = apiGetArg('q');
+    $feed = getFeed($feeds, apiGetArg('channel', 0));
+    
+    if ($searchTerms && $feed) {
+      $lastStoryId = apiGetArg('storyId', null);
+      if ($lastStoryId) {
+        // TODO: this has not been handled yet. I need more info on when this is used
+        break;
+      }
       
-       $lastStoryId = isset($_REQUEST['storyId']) ? $_REQUEST['storyId'] : NULL;
-       if ($lastStoryId) {
-          $index = $feed->getIndexForItem($lastStoryId);
-          if (is_null($index)) {
-            $index = 0;
-          } else {
-            $index++;
-          }
-       } else {
-          $index = 0;
-       }
-       
+      $feed->addFilter('search', $searchTerms);
+      $content = $feed->getData(); // this returns everything.......
+    }       
+    break;
+    
+  default:
+    $feed = getFeed($feeds, apiGetArg('channel', 0));
+    
+    if ($feed) {
+      $index = 0;
+      $lastStoryId = apiGetArg('storyId', null);
+      if ($lastStoryId) {
+        $feedIndex = $feed->getIndexForItem($lastStoryId);
+        if (!is_null($feedIndex)) {
+          $index = $feedIndex + 1;
+        }
+      }
+      
       $limit = 10;
       $content = $feed->getRSSItems($index, $limit);
+    }
+    break;
 }
 
 header('Content-Length: ' . strlen($content));
--- future/lib/api/people.php
+++ future/lib/api/people.php
@@ -1,42 +1,45 @@
 <?php
 
+/****************************************************************
+ *
+ *  Copyright 2010 The President and Fellows of Harvard College
+ *  Copyright 2010 Modo Labs Inc.
+ *
+ *****************************************************************/
+
+require_once realpath(LIB_DIR.'/api.php');
+
 $feedData = null;
 
-$feedConfigFile = realpath_exists(SITE_CONFIG_DIR."/feeds/people.ini");
-if ($feedConfigFile) {
-  $feedConfig = parse_ini_file($feedConfigFile, true);
-  
-  if ($feedConfig && isset($feedConfig['people'])) {
-    $feedData = $feedConfig['people'];
-  }
-}
-
-if (!$feedData) {
+$feedConfig = $GLOBALS['siteConfig']->loadFeedData('people');
+if (!$feedConfig || !isset($feedConfig['people'])) {
   $result = array('error' => 'Nothing Found');
   $content = json_encode($result);
   header('Content-Length: ' . strlen($content));
   echo $content;
-  exit;
+  exit;  
 }
+$feedData = $feedConfig['people'];
 
-$displayFields = $GLOBALS['siteConfig']->getAPIVar($_REQUEST['module'], 'displayFields');
+$displayFields = $GLOBALS['siteConfig']->getAPIVar(apiGetArg('module'), 'displayFields');
 
-switch ($_REQUEST['command']) {
+switch (apiGetArg('command')) {
   case 'details':
-    if (isset($_REQUEST['uid'])) {
-
+    $uid = apiGetArg('uid');
+    
+    if ($uid) {
       $PeopleController = PeopleController::factory($feedData);
       $PeopleController->setAttributes(array_keys($displayFields));
-      if ($person = $PeopleController->lookupUser($_REQUEST['uid'])) {
-            $result = array(
-                'uid'=>$person->getId()
-            );
-            foreach ($displayFields as $field=>$display) {
-                if ($value = $person->getField($field)) {
-                    $result[$field] = $value;
-                }
-            }
-            $content = json_encode($result);      
+      if ($person = $PeopleController->lookupUser($uid)) {
+        $result = array(
+          'uid' => $person->getId()
+        );
+        foreach ($displayFields as $field=>$display) {
+          if ($value = $person->getField($field)) {
+            $result[$field] = $value;
+          }
+        }
+        $content = json_encode($result);      
       } else {
         $result = array('error' => $ldap->gerError());
         $content = json_encode($result);
@@ -45,45 +48,49 @@ switch ($_REQUEST['command']) {
     }
     break;
   case 'search':
-    if (isset($_REQUEST['q']) && strlen((trim($_REQUEST['q'])))) {
-          $searchText = trim(stripslashes($_REQUEST['q']));
-          $PeopleController = PeopleController::factory($feedData);
-          $PeopleController->setAttributes(array_keys($displayFields));
-          
-          $people = $PeopleController->search($searchText);
-          if (!is_array($people)) {
-            $result = array('error' => 'Nothing Found');
-            $content = json_encode($result);
-          } elseif ($PeopleController->getError()) {
-            $result = array('error' => $PeopleController->getError());
-            $content = json_encode($result);
-        } elseif (count($people)==0) {
-            $result = array('error' => 'Nothing Found');
-            $content = json_encode($result);
-        } else {
-            
-            $results = array();
-            foreach ($people as $person) {
-                $result = array(
-                    'uid'=>$person->getId()
-                );
-                foreach ($displayFields as $field=>$display) {
-                    if ($value = $person->getField($field)) {
-                        $result[$field] = $value;
-                    }
-                }
-                
-                $results[] = $result;
-            }
-            
-            $content = json_encode($results);
+    $searchText = apiGetArg('q');
+    
+    if ($searchText) {
+      $PeopleController = PeopleController::factory($feedData);
+      $PeopleController->setAttributes(array_keys($displayFields));
+      
+      $people = $PeopleController->search($searchText);
+      if (!is_array($people)) {
+        $result = array('error' => 'Nothing Found');
+        $content = json_encode($result);
         
+      } elseif ($PeopleController->getError()) {
+        $result = array('error' => $PeopleController->getError());
+        $content = json_encode($result);
+        
+      } elseif (count($people)==0) {
+          $result = array('error' => 'Nothing Found');
+          $content = json_encode($result);
+          
+      } else {
+        $results = array();
+        foreach ($people as $person) {
+          $result = array(
+            'uid' => $person->getId()
+          );
+          foreach ($displayFields as $field=>$display) {
+            if ($value = $person->getField($field)) {
+              $result[$field] = $value;
+            }
+          }
+          
+          $results[] = $result;
         }
+        
+        $content = json_encode($results);
+      }
     }
     break;
+    
   case 'displayFields':
     $content = json_encode($displayFields);
     break;
+    
   default:
     break;
 }
