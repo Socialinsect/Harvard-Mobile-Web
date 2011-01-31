7d82a3a46f87746f537d262a53bf66dbff9987d0
Alexandra Ellwood <alexandra.ellwood@modolabs.com>
2011-01-31 18:05:46 -0500

    Removed unused Javascript.  Fixed typo in css

--- future/templates/modules/home/javascript/index-tablet.js
+++ future/templates/modules/home/javascript/index-tablet.js
@@ -1,15 +1,3 @@
-// Initalize the ellipsis event handlers
-/*clipWithEllipsis(function () {
-    
-    var elems = [];
-    for (var i = 0; i < 100; i++) { // cap at 100 divs to avoid overloading phone
-        var elem = document.getElementByTagName('ellipsis_'+i);
-        if (!elem) { break; }
-        elems[i] = elem;
-    }
-    return elems;
-});*/
-
 
 function moduleHandleWindowResize() {
   var blocks = document.getElementById('fillscreen').childNodes;
